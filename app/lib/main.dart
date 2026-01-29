3b47da53bcbc5fc658f9ae089aa4be73462d3b7d
Himanshu Pal <quickpressksj@gmail.com>
2026-01-29 11:35:01 +0530

    Update main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'widgets/bottom_navbar.dart';
 
+void main() {
+  runApp(const QuickPressApp());
+}
+
+class QuickPressApp extends StatelessWidget {
+  const QuickPressApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'QuickPress',
+      theme: ThemeData(
+        primaryColor: Colors.yellow,
+        scaffoldBackgroundColor: Colors.white,
+        useMaterial3: true,
+      ),
+      home: const BottomNavBar(),
+    );
+  }
+}
